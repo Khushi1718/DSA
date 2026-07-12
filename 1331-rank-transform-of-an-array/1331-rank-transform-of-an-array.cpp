18a7a2a0d25cc7d56f9a21441d5c5fa7f796700f
Khushi Nain <khushinain78@gmail.com>
2026-07-12 10:03:30 +0530

    Time: 20 ms (96.95%), Space: 39.8 MB (89.54%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 0 ) return{};
+        vector<pair<int,int>> vec;
+        
+        for(int i=0;i<n;i++)
+            vec.push_back({arr[i],i});
+
+        sort(vec.begin(),vec.end());
+        vector<int> ans(n);
+        int rank=1;
+        ans[vec[0].second]=1;
+        for(int i=1;i<n;i++){
+        if(vec[i].first!=vec[i-1].first)
+        rank++;
+
+        ans[vec[i].second]=rank;
+        }
+        return ans;
+    }
+};
