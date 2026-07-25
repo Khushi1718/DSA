faba0d25671d4b84cdf1d88a9a3c11b8cbdfc978
Khushi Nain <khushinain78@gmail.com>
2026-07-25 21:07:00 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.88%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,19 +1,28 @@
+// class Solution {
+// public:
+//     int maxProduct(int n) {
+//         int max1 = -1;
+//         int max2 = -1;
+
+//         while (n > 0) {
+//             int curr = n%10;
+//             n /= 10;
+//             if (curr>= max1) {
+//                 max2 = max1;
+//                 max1 = curr;
+//             } else if (curr> max2) {
+//                 max2 = curr;
+//             }
+//         }
+//         return max1*max2;
+//     }
+// };
 class Solution {
 public:
     int maxProduct(int n) {
-        int max1 = -1;
-        int max2 = -1;
-
-        while (n > 0) {
-            int curr = n%10;
-            n /= 10;
-            if (curr>= max1) {
-                max2 = max1;
-                max1 = curr;
-            } else if (curr> max2) {
-                max2 = curr;
-            }
-        }
-        return max1*max2;
+        string s = to_string(n);
+        sort(s.rbegin() , s.rend());
+        int maxProduct = (s[0]-'0')*(s[1]-'0');
+        return maxProduct;
     }
 };
