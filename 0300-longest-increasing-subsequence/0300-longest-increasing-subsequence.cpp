0ba471b2292362418d6ced998a11576806eda00d
Khushi Nain <khushinain78@gmail.com>
2026-08-31 13:39:34 +0530

    Time: 0 ms (100%), Space: 14.2 MB (77.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    vector<int> temp;
-    int lengthFrom(int i, vector<int>& nums) {
-        if (temp[i] != -1) return temp[i]; 
-        int best = 1; 
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[j] > nums[i]) {
-                best = max(best, 1 + lengthFrom(j, nums));
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tails;
+        for(int n : nums){
+            auto it = lower_bound(tails.begin(),tails.end(),n);
+            if(it == tails.end()){
+                tails.push_back(n);
+            }
+            else{
+                *it = n;
             }
         }
-        temp[i] = best; 
-        return best;
-    }
-
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        temp.assign(n, -1);
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, lengthFrom(i, nums));
-        }
-        return ans;
+        return tails.size();
     }
 };
